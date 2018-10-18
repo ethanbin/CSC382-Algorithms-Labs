cb618ad99b0b434ce115f7b1293b4a56873451e1
Ethan <ethanbinya@aol.com>
2018-10-17 21:30:40 -0400

    Started implementing quicksort

--- src/lab2/QuickSorterTime.java
+++ src/lab2/QuickSorterTime.java
@@ -3,8 +3,23 @@ package lab2;
 import Sorters.SorterTime;
 
 public class QuickSorterTime extends SorterTime {
+    // returns position of pivot used
+    private int partition(int[] arr, int start, int end){
+        int pivot = arr[end-1];
+        
+        return -1;
+    }
+
+    public void quickSort(int[] arr, int start, int end){
+        if (start < end){
+            int pivot = partition(arr, start, end);
+            quickSort(arr, start, pivot-1);
+            quickSort(arr,pivot+1, end);
+        }
+    }
+
     @Override
     public void sort(int[] arr) {
-
+        quickSort(arr, 0, arr.length);
     }
 }
